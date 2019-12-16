658c77838a4dd677fabe0c4b8cb7805bf2872c4d
BL-CoderOvertime <akutenshi@live.com>
2019-12-16 01:39:54 -0500

    setup Network Connectivity Checker with tests

--- clean_architecture_tdd/lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ clean_architecture_tdd/lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,12 +1,14 @@
 import 'package:clean_architecture_tdd/core/error/exceptions.dart';
 import 'package:clean_architecture_tdd/core/error/failure.dart';
-import 'package:clean_architecture_tdd/core/platform/network_info.dart';
+import 'package:clean_architecture_tdd/core/network/network_info.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/data/datasources/NumberTriviaLocalDataSource.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
+
+typedef Future<NumberTrivia> _ConcreteOrRandomChooser();
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   final NumberTriviaRemoteDataSource remoteDataSource;
@@ -34,7 +36,7 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   }
 
   Future<Either<Failure, NumberTrivia>> _getTrivia(
-      Future<NumberTrivia> Function() getConcreteOrRandom
+      _ConcreteOrRandomChooser getConcreteOrRandom
       ) async {
     if (await networkInfo.isConnected) {
       try {
