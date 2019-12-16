e5694e6ab237da817314372c55253bb7b1c40d9a
BL-CoderOvertime <akutenshi@live.com>
2019-12-16 03:27:56 -0500

    setup SharedPreferences for local storage, and as always... with tests

--- clean_architecture_tdd/lib/features/number_trivia/data/datasources/NumberTriviaLocalDataSource.dart
+++ clean_architecture_tdd/lib/features/number_trivia/data/datasources/NumberTriviaLocalDataSource.dart
@@ -1,12 +1,45 @@
-import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
 
-abstract class NumberTriviaLocalDataSource{
+import 'package:clean_architecture_tdd/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
+abstract class NumberTriviaLocalDataSource {
   ///Gets the cached [NumberTriviaModel] which was gotten the
   ///last time the user had internet connection
   ///
   /// Throws [CacheException] if no cached data is present.
   Future<NumberTriviaModel> getLastNumberTrivia();
 
-
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+   return  sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      jsonEncode(triviaToCache.toJson()),
+    );
+  }
 }
